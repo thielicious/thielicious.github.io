bd36d1eaa47062d0caef481d317e96e0be89e065
Thielicious <michel.thiel@gmx.net>
2016-12-15 12:03:13 +0100

    IC

--- scr/general.js
+++ scr/general.js
@@ -1,24 +1,24 @@
 // DOM-ready code
 
+function AiO(api,list,doc,sb,docs) {									
+	loadAPI(list,doc,api+'/'+api.toLowerCase()+'.htm');					// loads content of the clicked API
+	loadDoc(sb,api,docs);												// loads docs of an API
+	loadHash('#'+api.toLowerCase(),list,'ul.api li a');					// loads hash link
+}
+
 $(function() {
 	soon();																// 'Soon'-Dialog
 	activeLnkMenu();													// active style of a menu link
 	activeLnk('sidebar');												// active style of a sidebar link
 	lnkJump($('html, body'),800);										// link jump scroll animation
 	
-	loadAPI(0,0,'swIMG/swimg.htm');										// loads content of the clicked API
-	loadDoc(0,'swIMG',['doc','clog','dls','demo','lic']);				// loads docs of an API
-	
-	loadAPI(5,2,'eMM/emm.htm');
-	loadDoc(8,'eMM',['doc','clog','dls','demo','lic']);	
-	
-	loadAPI(6,1,'aLocal/alocal.htm');
-	loadDoc(5,'aLocal',['doc','dls','lic']);
-
 	stay();																// prevents scrolling to the top
 	toggleCode();														// toggle code preview
-
-	loadHash('#swimg',0,'ul.api li a');									// loads hash link
-	loadHash('#emm',5,'ul.api li a');
-	loadHash('#alocal',6,'ul.api li a');
+	
+	AiO('swIMG',0,0,0,['doc','clog','dls','demo','lic']);
+	AiO('unPOSTer',2,5,21,['doc','dls','lic']);
+	AiO('Dynamizer',3,3,13,['doc','clog','dls','demo','lic']);
+	AiO('eMM',5,2,8,['doc','clog','dls','demo','lic']);
+	AiO('aLocal',6,1,5,['doc','dls','lic']);
+	AiO('aLoad',7,4,18,['doc','dls','lic']);
 });
